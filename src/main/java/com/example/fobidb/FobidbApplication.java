2d22c6d61f8879de467317b3e4e69e77a77eb64e
“simon <simon.schubert@aloberlin.de>
2024-09-08 10:04:17 +0200

    - Init Commit - https://www.youtube.com/watch?v=9SGDpanrc8U - Bis 23:20 umgesetzt

--- src/main/java/com/example/fobidb/FobidbApplication.java
+++ src/main/java/com/example/fobidb/FobidbApplication.java
@@ -1,31 +1,13 @@
 package com.example.fobidb;
 
-import com.example.fobidb.teacher.Teacher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class FobidbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FobidbApplication.class, args);
 	}
-@GetMapping
-	public List<Teacher> hello() {
-		return List.of(
-				new Teacher(
-						1,
-						"Schubert",
-						"Simon",
-						"sb",
-						"simon.schubert@aloberlin.de",
-						45
-				)
-		);
-	}
+
 }
--- src/main/java/com/example/fobidb/teacher/TeacherController.java
+++ src/main/java/com/example/fobidb/teacher/TeacherController.java
@@ -1,4 +1,27 @@
 package com.example.fobidb.teacher;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/teacher")
 public class TeacherController {
+
+    @GetMapping
+    public List<Teacher> getTeachers() {
+        return List.of(
+                new Teacher(
+                        1,
+                        "Schubert",
+                        "Simon",
+                        "sb",
+                        "simon.schubert@aloberlin.de",
+                        45
+                )
+        );
+    }
+
 }
